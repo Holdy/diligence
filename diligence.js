0370ed969ca7cb7f6382683a0a90d1445dd82639
Chris Holden <chris@snoop.app>
2020-06-17 11:19:34 +0100

    Added unit tests for .json

---
+++ diligence.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+
+const logging = require('./lib/logging');
+const environment = require('./lib/environment');
+const ensure = require('./lib/ensure');
+const contentMap = require('./lib/contentMap');
+const awsSecretsManager = require('./lib/awsSecretsManager');
+const pipeline = require('./lib/pipeline');
+const json = require('./lib/json');
+const awsS3 = require('./lib/awsS3');
+const awsLambda = require('./lib/awsLambda');
+const postgres = require('./lib/postgres/postgres');
+
+const x = environment.getText({ name: 'SDLC_ENV', allow: ['dev'] });
+
+function readTextFile(relativePath) {
+    ensure.defined(relativePath, 'readTextFile - no file path was given');
+    try {
+        logging.logInfo(`Reading file: ${relativePath}`);
+        let content = fs.readFileSync(relativePath).toString();
+
+        let metadata = { fileName: relativePath };
+        contentMap.set(content, metadata);
+
+        return content;
+    } catch (err) {
+        logging.logAndThrowError(`Error while reading file: ${relativePath}`, err);
+    }
+}
+
+module.exports.logError = logging.logError;
+module.exports.environment = environment;
+module.exports.logging = logging;
+module.exports.readTextFile = readTextFile;
+module.exports.awsSecretsManager = awsSecretsManager;
+module.exports.pipeline = pipeline;
+module.exports.json = json;
+module.exports.awsS3 = awsS3;
+module.exports.awsLambda = awsLambda;
+module.exports.postgres = postgres;
+module.exports.ensure = ensure;
